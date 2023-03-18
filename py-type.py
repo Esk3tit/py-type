6d3b1d487ddfbd18dc539951bed3370be536f71d
Khai Phan <khaiphn41@gmail.com>
2023-03-17 23:28:50 -0700

    Improved random string generation by allowing users to enter the keys they want.
    Improved representation of spaces so that there is an actual symbol for the space displayed so you know if you need to type a space or not since previously there was no indication of spaces or having typed a space correctly.

--- py-type.py
+++ py-type.py
@@ -20,26 +20,51 @@ def start_screen(stdscr):
 
 
 def display_text(stdscr, target, current, wpm=0):
-    stdscr.addstr(target)
-    stdscr.addstr(1, 0, f"WPM: {wpm}")
+    # Add special unicode symbol representations for spaces and new lines
+    for char in target:
+        target_char = char
+        if char == ' ':
+            target_char = '\u2423'
+
+        stdscr.addstr(target_char)
+
+    stdscr.addstr(f"\nWPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
-        stdscr.addstr(0, i, char, curses.color_pair(1) if char == correct_char else curses.color_pair(2))
+        color = curses.color_pair(3)
+        if char == correct_char and correct_char == ' ':
+            color = curses.color_pair(4)
+        elif char == correct_char:
+            color = curses.color_pair(1)
+        else:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, i, char, color)
 
 
-def load_text():
+def get_keys_to_practice():
+    keys = input("Enter the letters you want in the typing prompt (no delimiter in between, ex. 'abCde./,'): ")
+    allowed_keys = [char for char in keys if char in string.printable]
+    return allowed_keys
+
+
+def load_text(allowed_keys, length=50):
     # generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
     # context = "Generate text using words with as many of the following letters as possible: e"
     # output = generator(context, max_length=50, do_sample=True, temperature=0.9)
     # print(output)
     # return output
-    return ''.join(random.choices(' ' + 'e', k=20))
+    return ''.join(random.choices(allowed_keys, k=length))
 
 
 def wpm_test(stdscr):
     # load_text()
-    target_text = load_text()
+    curses.def_prog_mode()
+    curses.reset_shell_mode()
+    allowed_keys = get_keys_to_practice()
+    curses.reset_prog_mode()
+    target_text = load_text(allowed_keys)
     current_text = []
     wpm = 0
     start_time = time.time()
@@ -75,9 +100,12 @@ def wpm_test(stdscr):
 
 
 def main(stdscr):
+    curses.def_shell_mode()
+
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_GREEN)
 
     start_screen(stdscr)
 
